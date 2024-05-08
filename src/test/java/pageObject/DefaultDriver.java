fcf0c0c4844248b95ee59ddb4c22d2694a67928d
Rajeev Rahi <rajeev.rahi32@gmail.com>
2024-05-09 00:05:58 +0200

    Updated defaultdriver

--- src/test/java/pageObject/DefaultDriver.java
+++ src/test/java/pageObject/DefaultDriver.java
@@ -36,7 +36,7 @@ public class DefaultDriver {
         String dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         TakesScreenshot screenshot = ((TakesScreenshot) webDriver);
         File source = screenshot.getScreenshotAs(OutputType.FILE);
-        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateFormat + ".png";
+        String destination = System.getProperty("user.dir") + "/test-output/Screenshots/" + screenshotName + dateFormat + ".png";
         File finalDestination = new File(destination);
         FileUtils.copyFile(source, finalDestination, true);
         return destination;
