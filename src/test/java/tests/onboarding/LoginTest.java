24a33d6203ec6538376e21563cc9871a4c4d31ec
Rajeev Rahi <rajeev.rahi32@gmail.com>
2024-05-08 17:33:57 +0200

    updated reporting

--- src/test/java/tests/onboarding/LoginTest.java
+++ src/test/java/tests/onboarding/LoginTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.*;
 import pageObject.DefaultDriver;
 import pageObject.HomeScreen;
 import pageObject.LoginScreen;
+import utility.SparkReporter;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,70 +20,48 @@ import java.util.Optional;
 
 public class LoginTest {
     private DefaultDriver defaultDriver;
-    private ExtentSparkReporter extentSparkReporter;
-    private ExtentReports extentReports;
-    private ExtentTest extentTest;
     private LoginScreen loginScreen;
     private HomeScreen homeScreen;
+    private SparkReporter sparkReporter;
+    private ITestResult iTestResult;
 
     @BeforeTest
     public void setupDriverAndReport() {
         defaultDriver = new DefaultDriver();
-        extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") +"/test-output/testReport.html");
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(extentSparkReporter);
-
-        extentSparkReporter.config().setOfflineMode(true);
-        extentSparkReporter.config().setDocumentTitle("Sample Automation Report");
-        extentSparkReporter.config().setReportName("Test Report");
-        extentSparkReporter.config().setTheme(Theme.STANDARD);
-        extentSparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
-        extentSparkReporter.config().setEncoding("UTF-8");
     }
 
     @BeforeMethod
     public void setupBrowser() {
         WebDriver webDriver = defaultDriver.launchURL(Optional.ofNullable(System.getProperty("browserMode")));
+        sparkReporter = new SparkReporter();
         loginScreen = new LoginScreen(webDriver);
         homeScreen = new HomeScreen(webDriver);
     }
 
     @Test(priority = 0)
     public void testLogin(){
-        extentTest = extentReports.createTest("Login Test", "Test Login functionality");
+        sparkReporter.initializeReportInTest("Login Test", "Test Login functionality");
         loginScreen.login();
         homeScreen.verifyHomeScreen();
     }
 
     @Test(priority = 1)
     public void testNearestBranch() {
-        extentTest = extentReports.createTest("Nearest Bank Branch Test", "Test Nearest Bank Branch functionality");
+        sparkReporter.initializeReportInTest("Nearest Bank Branch Test", "Test Nearest Bank Branch functionality");
         loginScreen.login();
         homeScreen.verifyHomeScreen();
         homeScreen.searchBranch();
     }
 
     @AfterMethod
-    public void getResult(ITestResult result) throws IOException {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            String absolutePath = "_";
-            extentTest.log(Status.FAIL, "Test Case Failed is "+result.getName());
-            extentTest.log(Status.FAIL, "Failed due to:-- "+result.getThrowable());
-            extentTest.log(Status.FAIL, "Checkout the screenshot below: ");
-            //Open report from finder NOT from intelij project.  File file = new File(System.getProperty("user.dir") + "/test-output/Screenshots/" + result.getName() + ".png");
-            File file = new File(defaultDriver.getScreenShot(result.getName()));
-            absolutePath = file.getAbsolutePath();
-            extentTest.log(Status.FAIL, MediaEntityBuilder.createScreenCaptureFromPath(absolutePath).build());
-        } else if (result.getStatus() == ITestResult.SUCCESS) {
-            extentTest.log(Status.PASS, result.getTestName());
-        } else {
-            extentTest.log(Status.SKIP, result.getTestName());
-        }
+    public void setTestResultAndCloseDriver(ITestResult iTestResult) throws IOException {
+        this.iTestResult = iTestResult;
+        sparkReporter.configureReporter(iTestResult.getStatus(), iTestResult.getName(), iTestResult.getThrowable(), defaultDriver);
         defaultDriver.tearDown();
     }
 
     @AfterTest
     public void closeReport() {
-        extentReports.flush();
+        sparkReporter.tearDownReport();
     }
 }
---
+++ src/test/java/utility/SparkReporter.java
@@ -0,0 +1,58 @@
+package utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.ITestResult;
+import pageObject.DefaultDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SparkReporter {
+    private ExtentSparkReporter extentSparkReporter;
+    private ExtentReports extentReports;
+    private ExtentTest extentTest;
+    DefaultDriver defaultDriver;
+
+    public SparkReporter() {
+        extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") +"/test-output/testReport.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentSparkReporter);
+
+        extentSparkReporter.config().setOfflineMode(true);
+        extentSparkReporter.config().setDocumentTitle("Sample Automation Report");
+        extentSparkReporter.config().setReportName("Test Report");
+        extentSparkReporter.config().setTheme(Theme.STANDARD);
+        extentSparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+        extentSparkReporter.config().setEncoding("UTF-8");
+    }
+    public void configureReporter(int testStatus, String testName, Throwable reason, DefaultDriver defaultDriver) throws IOException {
+        this.defaultDriver = defaultDriver;
+        if (testStatus == ITestResult.FAILURE) {
+            String absolutePath = "_";
+            extentTest.log(Status.FAIL, "Test Case Failed is " + testName);
+            extentTest.log(Status.FAIL, "Failed due to:-- " + reason);
+            extentTest.log(Status.FAIL, "Checkout the screenshot below: ");
+            //Open report from finder NOT from intelij project.  File file = new File(System.getProperty("user.dir") + "/test-output/Screenshots/" + result.getName() + ".png");
+            File file = new File(defaultDriver.getScreenShot(testName));
+            absolutePath = file.getAbsolutePath();
+            extentTest.log(Status.FAIL, MediaEntityBuilder.createScreenCaptureFromPath(absolutePath).build());
+        } else if (testStatus == ITestResult.SUCCESS) {
+            extentTest.log(Status.PASS, "The test " +testName +" has been successfully executed.");
+        } else {
+            extentTest.log(Status.SKIP, "Unfortunately the test " +testName +" was skipped.");
+        }
+    }
+    public void initializeReportInTest(String testName, String testDescription) {
+        extentTest = extentReports.createTest(testName, testDescription);
+    }
+
+    public void tearDownReport() {
+        extentReports.flush();
+    }
+
+}
